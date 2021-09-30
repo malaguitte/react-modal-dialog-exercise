4340793200a52d80a1f747352208976be4e994f3
Anderson Malaguitte <anderson@Andersons-MacBook-Pro.local>
2021-10-01 01:09:40 +0200

    Test button label as a variable

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import './App.scss'
 import { useState } from 'react';
 import RegistrationDialog from './components/registration-dialog/RegistrationDialog';
 
+const TEST_BUTTON_LABEL = "Show/Hide Dialog";
 const onCancelClick = () => console.log("Clicked Cancel");
 const onOkClick = () => console.log("Clicked OK");
 
@@ -10,7 +11,7 @@ function App() {
 
   return (
     <div className="App">
-      <button onClick={() => setActive(!isActive)}>Show/Hide Dialog</button>
+      <button onClick={() => setActive(!isActive)}>{TEST_BUTTON_LABEL}</button>
       <RegistrationDialog
         isActive={isActive}
         onHandleCancelClick={onCancelClick}
