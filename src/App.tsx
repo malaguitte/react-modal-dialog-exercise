3df5eb30dec5b61ea1ae2da22103d7d9ec52622c
Anderson Malaguitte <anderson@Andersons-MacBook-Pro.local>
2021-10-01 17:05:20 +0200

    Draggable layer

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import './App.scss'
 import { useState } from 'react';
 import RegistrationDialog from './components/registration-dialog/RegistrationDialog';
+import { dragElement } from "./utils/Drag";
 
 const TEST_BUTTON_LABEL = "Show/Hide Dialog";
 const onCancelClick = () => console.log("Clicked Cancel");
@@ -8,6 +9,12 @@ const onOkClick = () => console.log("Clicked OK");
 
 function App() {
   const [isActive, setActive] = useState(false);
+
+  // This makes the layer element draggable.
+  const layerElement = document.getElementById("layer_content");
+  if (layerElement) {
+    dragElement(layerElement);
+  }
 
   return (
     <div className="App">
---
+++ src/utils/Drag.tsx
@@ -0,0 +1,41 @@
+export function dragElement(element: HTMLElement) {
+    let pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+    const headerElement = document.getElementById(`${element.id}_header`);
+    if (headerElement) {
+      // if present, the header is where you move the DIV from:
+      headerElement.onmousedown = dragMouseDown;
+    } else {
+      // otherwise, move the DIV from anywhere inside the DIV:
+      element.onmousedown = dragMouseDown;
+    }
+
+    function dragMouseDown(event: MouseEvent): void {
+      const e = event ?? window.event;
+      e.preventDefault();
+      // get the mouse cursor position at startup:
+      pos3 = e.clientX;
+      pos4 = e.clientY;
+      document.onmouseup = stopDragElement;
+      // call a function whenever the cursor moves:
+      document.onmousemove = startDragElement;
+    }
+  
+    function startDragElement(event: MouseEvent): void {
+      const e = event ?? window.event;
+      e.preventDefault();
+      // calculate the new cursor position:
+      pos1 = pos3 - e.clientX;
+      pos2 = pos4 - e.clientY;
+      pos3 = e.clientX;
+      pos4 = e.clientY;
+      // set the element's new position:
+      element.style.top = (element.offsetTop - pos2) + "px";
+      element.style.left = (element.offsetLeft - pos1) + "px";
+    }
+  
+    function stopDragElement(): void {
+      // stop moving when mouse button is released:
+      document.onmouseup = null;
+      document.onmousemove = null;
+    }
+  }
